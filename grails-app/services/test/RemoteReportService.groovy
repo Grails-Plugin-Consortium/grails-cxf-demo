a829770f34fe862ccabe96754080dec48d68c32e
ctoestreich <acetrike@yahoo.com>
2012-11-01 13:20:38 -0500

    checking in additional method

--- grails-app/services/test/RemoteReportService.groovy
+++ grails-app/services/test/RemoteReportService.groovy
@@ -4,6 +4,8 @@ import org.grails.cxf.utils.EndpointType
 
 import javax.jws.WebMethod
 import javax.jws.WebResult
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter
+import org.grails.cxf.adapter.GrailsCxfMapAdapter
 
 class RemoteReportService {
 
@@ -16,5 +18,12 @@ class RemoteReportService {
         reports << RemoteReport.findOrSaveWhere(reportId: 1L, errorCode: 1L, errorMessage: 'Binders full of women.')
         reports << RemoteReport.findOrSaveWhere(reportId: 2L, errorCode: 1L, errorMessage: 'Binder?! I hardly know her!')
         reports
+    }
+
+    @XmlJavaTypeAdapter(GrailsCxfMapAdapter.class)
+    @WebResult(name='entires')
+    @WebMethod(operationName = 'getMaps')
+    Map<String, String> getMeMaps() {
+        return ['Colombia': 'Yummy', 'Ethiopia': 'Drink']
     }
 }
